d8d47e0786cfeb8b28cb4aeb145d31625f1b48c4
Jayanth <jayanth.vishwas@bizcloudexperts.com>
2024-05-06 15:03:14 -0500

    data to dynamo

--- src/realtime/ordersRT.py
+++ src/realtime/ordersRT.py
@@ -1,5 +1,6 @@
 import boto3
 import pandas as pd
+import json
 import io
 
 
@@ -15,8 +16,34 @@ def getFileFromS3(bucketName, s3key):
         print("Error in getFileFromS3: ", e)
         raise Exception("Error getting query from S3: ") from e
         
+def _convert_value(val):
+    """ Convert Python data types to DynamoDB-compatible types. """
+    if isinstance(val, str):
+        return val
+    elif isinstance(val, int) or isinstance(val, float):
+        return str(val)
+    elif isinstance(val, bool):
+        return val
+    elif isinstance(val, list):  # Lists of strings or numbers
+        return val
+    elif isinstance(val, dict):  # Nested dictionaries
+        return val
+    else:
+        return val
+        
+def write_df_to_dynamodb(df, table_name):
+    table = boto3.resource('dynamodb', region_name='us-east-1').Table(table_name)
+    items = df.apply(lambda x: json.loads(x.to_json()), axis=1)
+    for item in items:
+        dynamo_item = {k: _convert_value(v) for k, v in item.items()}
+        try:
+            response = table.put_item(Item=dynamo_item)
+            print("Successfully inserted item:", dynamo_item)
+        except Exception as e:
+            print("Error inserting item:", dynamo_item, "Error:", e)
+
+        
 def handler(event, context):
-    print("hello")
     print(event)
     records = event.get('Records')[0]
     s3Key = records['s3']['object']['key']
@@ -26,5 +53,10 @@ def handler(event, context):
     df_sorted = df.sort_values(by=['id', 'transact_id'], ascending = [True, False])
 
     df_unique = df_sorted.drop_duplicates(subset='id', keep='first')
-
-    print(df_unique[['id', 'transact_id', 'blnum']])
+    
+    print(df_sorted[['id', 'transact_id']])
+    print(df_unique[['id', 'transact_id']])
+    
+    write_df_to_dynamodb(df_unique, 'omni-live-orders-dev')
+    print("Completed")
+    
